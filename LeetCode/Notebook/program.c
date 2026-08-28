939be64a2d1b4c60b4ef17d81bbed10580ba1056
francogiof <abel.ccapa@tecsup.edu.pe>
2026-08-28 13:47:37 -0500

    vault backup: 2026-08-28 13:47:37

--- LeetCode/Notebook/program.c
+++ LeetCode/Notebook/program.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int* result = malloc(2 * sizeof(int));
+int *twoSum(const int *nums, int numsSize, int target, int *returnSize) {
+    int *result = malloc(2 * sizeof(*result));
+    if (result == NULL) {
+        *returnSize = 0;
+        return NULL;
+    }
 
     for (int i = 0; i < numsSize; i++) {
         for (int j = i + 1; j < numsSize; j++) {
@@ -15,16 +19,15 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
         }
     }
 
-    *returnSize = 0;
     free(result);
+    *returnSize = 0;
     return NULL;
 }
 
 int main(void) {
     int nums[] = {2, 7, 11, 15};
-    int returnSize;
-
-    int* result = twoSum(nums, 4, 9, &returnSize);
+    int returnSize = 0;
+    int *result = twoSum(nums, 4, 9, &returnSize);
 
     if (result != NULL) {
         printf("[%d, %d]\n", result[0], result[1]);
